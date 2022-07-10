e6f1a1f0ba264997e5b9d858957a2b4d539b3c85
PlouffeLuL <56097826+PlouffeLuL@users.noreply.github.com>
2022-07-09 22:01:43 -0400

    idk anymore

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Start
+            Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
